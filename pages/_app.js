c5911bc012c48550774329e0eb7b292cf756be7c
uufukttas <uufukttas@hotmail.com>
2021-12-03 00:39:29 +0300

    All portfolio sites uploaded.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import 'tailwindcss/tailwind.css'
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return <>
+            <Navigation/>
+            <Component {...pageProps} />
+            <Footer/>
+    </>
 }
 
 export default MyApp
