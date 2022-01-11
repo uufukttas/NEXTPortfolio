391d45c2e547163a661072e319f05a9c554a4704
uufukttas <uufukttas@hotmail.com>
2022-01-12 00:50:04 +0300

    GTAG Script added.

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,19 @@ export default function Home() {
       <Head>
         <title>UFUK TAS</title>
         <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RKS4T2VK5M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RKS4T2VK5M');
+          `}
+        </Script>
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">
