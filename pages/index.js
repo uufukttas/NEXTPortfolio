66822f101eafabf16822556e4810fce1be37e841
uufukttas <uufukttas@hotmail.com>
2022-01-23 22:44:26 +0300

    React-GA package added.

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,20 @@
 import Head from 'next/head'
-import Script from 'next/script'
 import About from "../Components/About";
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
 
 export default function Home() {
 
-  const gtag = () => {
-    window.dataLayer.push(arguments)
-  }
+    useEffect(() => {
+      ReactGA.initialize('G-RKS4T2VK5M');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [])
 
   return (
     <div className="space-y-14 lg:space-y-24">
       <Head>
         <title>UFUK TAS</title>
         <link rel="icon" href="/favicon.ico" />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RKS4T2VK5M"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {
-            typeof window !== 'undefined'
-            ? window.dataLayer = window.dataLayer || []
-              && gtag('js', new Date())
-              && gtag('config', 'G-RKS4T2VK5M')
-            : null
-          }
-        </Script>
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">
