606a165408128b2b88dce28c0a7cd531ef7106cc
uufukttas <uufukttas@hotmail.com>
2022-01-23 22:47:57 +0300

    React-GA package added.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import About from "../Components/About";
 import ReactGA from 'react-ga';
-import { useEffect } from 'react';
 
 export default function Home() {
 
