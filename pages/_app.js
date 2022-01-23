61ccb532b7ce897ab721093a8d25518861fc804f
uufukttas <uufukttas@hotmail.com>
2022-01-23 23:46:27 +0300

    ReactGA added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,17 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import React, { useEffect } from 'react'
 import ReactGA from 'react-ga'
 import Navigation from "../Components/Navigation";
 import Footer from "../Components/Footer";
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        ReactGA.initialize('G-RKS4T2VK5M');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    });
+
     return <>
-            <div>
-            <ReactGA.OutboundLink
-                eventLabel="myLabel"
-                to="http://www.example.com"
-                target="_blank">
-                My Link
-                </ReactGA.OutboundLink>
-            </div>
             <Navigation/>
             <Component {...pageProps} />
             <Footer/>
