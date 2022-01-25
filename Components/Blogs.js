44758c8237e9151ff6d775b1a32fd086888d891b
uufukttas <uufukttas@hotmail.com>
2022-01-26 00:01:47 +0300

    Blog Page Component Edited.

--- Components/Blogs.js
+++ Components/Blogs.js
@@ -4,7 +4,17 @@ import { XMLParser, XMLBuilder} from 'fast-xml-parser';
 function Blogs() {
 
   useEffect(() => {
-    fetch("https://medium.com/feed/@uufukttas")
+    fetch("https://medium.com/feed/@uufukttas", {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer'})
       .then(res => res.text())
       .then(data => {
         const xml = parser.parse(data);
