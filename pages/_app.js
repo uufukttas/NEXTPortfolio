9e98d56497380e178e857e6facb63a495e613e9b
uufukttas <uufukttas@hotmail.com>
2022-03-12 23:35:10 +0300

    GTAG function moved to useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,32 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import React from 'react'
+import Router from "next/router"
+import { route } from 'next/dist/server/router';
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import * as ga from '../Components/ga/lib/Analytics'
 import Navigation from "../Components/Navigation";
 import Footer from "../Components/Footer";
 
 function MyApp({ Component, pageProps }) {
-    return <>
-            <Navigation/>
-            <Component {...pageProps} />
-            <Footer/>
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events]);
+
+    return 
+    <>
+      <Navigation/>
+      <Component {...pageProps} />
+      <Footer/>
     </>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
 import { useEffect } from 'react'
 import Head from 'next/head'
 import About from "../Components/About";
-import ReactGA from 'react-ga';
 
 export default function Home() {
-
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [] 
-    gtag('js', new Date()) 
-    gtag('config', 'UA-217922925-1') 
-  }, []);
-
-  const gtag = () => {
-    dataLayer.push(arguments);
-  }
-
   return (
     <div className="space-y-14 lg:space-y-24">
       <Head>
