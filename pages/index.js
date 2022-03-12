d46f87dcab96fad773ca4f4b3efa42d2b0df236b
uufukttas <uufukttas@hotmail.com>
2022-03-12 23:08:05 +0300

    GTAG Function added.

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,23 @@ export default function Home() {
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
+  const gtag = () => {
+    dataLayer.push(arguments);
+  }
+
   return (
     <div className="space-y-14 lg:space-y-24">
       <Head>
         <title>UFUK TAS</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-217922925-1"></script>
+            { window.dataLayer = window.dataLayer || [] }
+            { gtag('js', new Date()) }
+            { gtag('config', 'UA-217922925-1') }
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">
-        <About/>
+        <About />
       </main>
     </div>
   )
