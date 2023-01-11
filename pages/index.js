908f29dbbb842178780a4e51787c72196893f6e6
UFUK TAS <uufukttas@hotmail.com>
2023-01-11 17:45:26 +0300

    Added lang properties

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import About from "../Components/About";
 export default function Home() {
   return (
     <div className="space-y-14 lg:space-y-24">
-      <Head>
+      <Head lang="en">
         <title>UFUK TAS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
