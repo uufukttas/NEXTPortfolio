be27db043327f3dadb5bd178d705ce3e921f15ad
UFUK TAS <uufukttas@hotmail.com>
2023-08-07 22:12:20 +0300

    Added About Page [Hidden]

--- Components/About.js
+++ Components/About.js
@@ -1,40 +1,27 @@
-import React from "react"
-import Image from "next/image"
-import profile from "../public/profile.jpeg"
+import React from 'react'
 
 const About = () => {
     return (
-        <main className="max-w-4xl mx-auto antialiased py-16 lg:py-36">
-            <div className="container px-4 mx-auto">
-                <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-                    <div className="lg:px-4 lg:mt-12 ">
-                        <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                            Hey there, I am Ufuk
-                        </h1>
-                        <div className="mt-6 text-gray-800 dark:text-white">
-                            <p className="mb-4">
-                                I am a dedicated, open-minded computer engineer who is passionate about state-of-art technologies. Besides, I am a lifelong learner that endeavors to expand my skillset.
-                                <br />
-                                <br />
-                                I am interested in Frontend Development using React, Tailwind, NextJS, Gatsby. Moreover, I know git, jira, netlify environments.
-                            </p>
+        <div className="max-w-4xl mx-auto antialiased lg:mt-0 py-8 lg:py-18">
+            <h2 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white text-left">About Me</h2>
+            <div className="container px-4 mx-auto flex">
+                <div className="mt-6 text-gray-800 dark:text-white text-center w-1/2">
+                    <h3 className='text-2xl font-bold text-gray-900 lg:text-xl dark:text-white text-left'>Experience</h3>
+                    <div className='experience-container ml-5 mt-5 flex items-center justify-center'>
+                        <span className='mx-1'>●</span>
+                        <div className='experience-item w-9/12 flex flex-col flex-start'>
+                            <div className='experience-item-company'>Jotform</div>
+                            <div className='experience-item-date'>2021 - Present</div>
+                            <div className='experience-item-title'>Frontend Developer</div>
                         </div>
                     </div>
-                    <div className="flex flex-shrink-0 lg:mt-12 lg:px-4 mb-10 justify-center items-center">
-                        <Image
-                            src={profile}
-                            alt="Profile"
-                            priority={true}
-                            className="rounded-full"
-                            width={250}
-                            height={250}
-                            placeholder="blur"
-                        />
-                    </div>
+                </div>
+                <div className='mt-6 text-gray-800 dark:text-white text-center w-1/2'>
+                    <h3 className='text-2xl font-bold text-gray-900 lg:text-xl dark:text-white text-right'>Education</h3>
                 </div>
             </div>
-        </main>
+        </div>
     )
 }
 
-export default About;
+export default About
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import About from '../Components/About'
+
+const about = () => {
+  return (
+    <About/>
+  )
+}
+
+export default about
